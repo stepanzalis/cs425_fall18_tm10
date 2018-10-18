46e07a8167d7728e1d1be9faf8d647eed5609fc7
Štěpán Záliš <stepan.zalis@gmail.com>
2018-10-18 20:29:12 +0300

    server changes

---
+++ Assignment1/src/main/java/ucy/cs425/ServerTCP.java
@@ -0,0 +1,34 @@
+package main.java.ucy.cs425;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class ServerTCP {
+
+    static long startTime = System.currentTimeMillis();
+    static long timeInterval = 1000;
+
+    public static void main(String[] args) {
+
+        final int port = Integer.parseInt(args[0]);
+        final int numberOfRepetition = Integer.parseInt(args[1]);
+
+        try {
+            ServerSocket socket = new ServerSocket(port);
+
+            while (true) {
+
+                Socket client = socket.accept();
+
+                // TODO: create multithreaded scheduler
+            }
+
+        } catch (IOException io) {
+            io.getLocalizedMessage();
+        }
+
+
+
+    }
+}
