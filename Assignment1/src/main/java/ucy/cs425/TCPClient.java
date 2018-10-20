84bcb4e2a7e7ffb347a0bbc1ed377988d720d59c
Štěpán Záliš <44142488+stepanzalis@users.noreply.github.com>
2018-10-20 20:37:21 +0300

    Refactored Client
    Refactored client:
    - renamed variables to coresponding to java conventions
    - small changs

--- Assignment1/src/main/java/ucy/cs425/TCPClient.java
+++ Assignment1/src/main/java/ucy/cs425/TCPClient.java
@@ -1,62 +1,50 @@
+package main.java.ucy.cs425;
 
 import java.io.*;
-import java.net.Inet4Address;
-import java.net.InetAddress;
 import java.net.Socket;
 import java.util.Arrays;
-import java.util.Date;
-import java.util.LinkedList;
-import java.util.Scanner;
-import java.io.FileWriter;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
-
-
 /**
- *This class simulates at minimum of 10 tcp clients who establishes a connection 
- *with a server hosted on a virtual machine and requesting data from him. It is implemented using 
- *multithreading code. Every tcp client is a thread and clients are running concurrently and every
- *client generate a series of requests towards the server.The client sends a hello message which comprises of
- *a "hello" string the client's ip and the port and the client id. 
+ * This class simulates at minimum of 10 tcp clients who establishes a connection
+ * with a server hosted on a virtual machine and requesting data from him. It is implemented using
+ * multithreading code. Every tcp client is a thread and clients are running concurrently and every
+ * client generate a series of requests towards the server.The client sends a hello message which comprises of
+ * "hello" string the client's ip and the port and the client id.
  *
- * @author
- *
+ * @author 
  */
-
 
 public class TCPClient {
 
     public static long id = 1;
-    static String serverIp;
-    static int port;
+    static private String serverIp;
+    static int port = 0;
 
-/**Inner class Client which extends threads and implements runnable interface for multithreading*/
+    /**
+     * Inner class Client which extends threads and implements runnable interface for multithreading
+     */
     static class Client extends Thread implements Runnable {
 
-/**When an object implementing interface Runnable is used to create a thread, starting the 
- *thread causes the object's run method to be called in that separately executing thread.
- *The general contract of the method run is that it may take any action whatsoever.
- */
+        /**
+         * When an object implementing interface Runnable is used to create a thread, starting the
+         * thread causes the object's run method to be called in that separately executing thread.
+         * The general contract of the method run is that it may take any action whatsoever.
+         */
         @Override
         public void run() {
             try {
-                FileWriter fout = new FileWriter("filename.txt", true);
-                //PrintWriter pw = new PrintWriter(fout);
 
-                String response, pac;
+                String response;
                 String message;
-                long[] latencytable;
-                latencytable = new long[300];
-                long senttime;
-                long receivetime;
+                long[] latencyTable = new long[300];
+
+                long sentTime;
+                long receiveTime;
                 int count = 0;
 
-                // CSVWriter writer = new CSVWriter(new FileWriter("yourfile.csv"), '\t');
-
-
                 Socket socket = new Socket(serverIp, port);
-                int i = 0;
 
                 DataOutputStream output = new DataOutputStream(socket.getOutputStream());
                 BufferedReader server = new BufferedReader(
@@ -64,55 +52,48 @@ public class TCPClient {
                 );
 
 
-                // int test = server.read();
+                final Lock mutex = new ReentrantLock(true);
 
-                final Lock _mutex = new ReentrantLock(true);
+                mutex.lock();
 
-                _mutex.lock();
-
-               /**protect code using a mutex because of multithreading (protect clients id variable)*/ 
+                // protect code using a mutex because of multithreading (protect clients id variable)
                 output.write((int) id);
                 long prID = id;
                 id++;
 
-                _mutex.unlock();
-
+                mutex.unlock();
 
                 while (true) {
 
-
                     message = "HELLO " + socket.getInetAddress() + " " + socket.getPort() + " " + prID + System.lineSeparator();
-                
-                    senttime = System.nanoTime();
+
+                    sentTime = System.nanoTime();
                     output.writeBytes(message);
 
                     response = server.readLine();
-                    receivetime = System.nanoTime();
-                    // latencytable[count]=receivetime-senttime;
+                    receiveTime = System.nanoTime();
 
-                    if (response.equals("end")) {
-                        
+                    if (response.equals("ByeBye!")) {
+
                         socket.close();
-                        System.out.println(Arrays.toString(latencytable));
+                        System.out.println(Arrays.toString(latencyTable));
                         int x = 0;
                         long sum = 0;
-                        while (latencytable[x] != 0) {
-                            sum = sum + latencytable[x];
+                        while (latencyTable[x] != 0) {
+                            sum = sum + latencyTable[x];
                             x++;
                             if (x == 300)
                                 break;
                         }
-                        
+
                         System.out.println("The Sum Latency is: " + sum / 1000000);
                         System.out.println("The Average Latency is: " + (sum / x) / 1000000);
 
-                        fout.write(String.valueOf((sum / x) / 1000000) + "\n");
-                        fout.close();
 
                         break;
                     }
                     System.out.println(response);
-                    latencytable[count] = receivetime - senttime;
+                    latencyTable[count] = receiveTime - sentTime;
                     count++;
                 }
 
@@ -120,34 +101,25 @@ public class TCPClient {
             } catch (IOException e) {
                 e.printStackTrace();
             }
-
-
         }
-
-
     }
 
-/**Main method reading and parsing from command line and starts the simulation
-  * To run the program execute java TCPClient _server's IP_ _Port_
-  *
-  * @param args arguments from command lines
-  *
-  *	
-  */
+    /**
+     * Main method reading and parsing from command line and starts the simulation
+     * To run the program execute java TCPClient _server's IP_ _Port_
+     *
+     * @param args arguments from command lines
+     */
     public static void main(String args[]) {
 
-        serverIp = args[0];
+        final int NUMBER_OF_CLIENTS = 10;
+
+        serverIp = (args[0]);
         port = Integer.parseInt(args[1]);
-        int i;
 
-
-        for (i = 0; i < 10; i++) {
+        for (int i = 0; i <= NUMBER_OF_CLIENTS; i++) {
             Client client = new Client();
             client.start();
-
         }
-
-
     }
-
 }
